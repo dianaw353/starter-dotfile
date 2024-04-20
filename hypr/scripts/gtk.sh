3883d146ae2094aef3acae4631b8d7761c3f06f7
Diana <diana.ward535@gmail.com>
2024-04-19 19:02:11 -0600

    update

--- hypr/scripts/gtk.sh
+++ hypr/scripts/gtk.sh
@@ -22,6 +22,7 @@ gsettings set "$gnome_schema" icon-theme "$icon_theme"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
 gsettings set "$gnome_schema" font-name "$font_name"
 gsettings set "$gnome_schema" color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.wm.preferences button-layout '' 
 
 if [ -f ~/dotfiles/hypr/conf/cursor.conf ] ;then
     echo "exec-once = hyprctl setcursor $cursor_theme $cursor_size" > ~/dotfiles/hypr/conf/cursor.conf
